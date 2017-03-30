a7e5924c5b8647ba5cc9fbe1de05e6088c5f9468
James Shephard <james@jshephard.net>
2017-03-30 14:24:16 -0700

    Load video

--- main.cpp
+++ main.cpp
@@ -4,89 +4,50 @@
 // using namespace std;
 
 int main(int argc, char** argv) {
-  if ( argc != 3 ){
-    printf("usage: DisplayImage.out <Image_Path> <Image_Path>\n");
-    return -1;
-  }
+    if (argc != 2) {
+        printf("usage: DisplayImage.out <Video Path>\n");
+        return -1;
+    }
 
-  Mat template_img_1;
-  template_img_1 = imread( argv[1], 1 );
+    VideoCapture video(argv[1]);
+    if (!video.isOpened()) {
+        printf("No video data\n");
+        return -1;
+    }
 
-  if ( !template_img_1.data ){
-    printf("No Template data \n");
-    return -1;
-  }
-
-  Mat template_img_2;
-  template_img_2 = imread( argv[2], 1 );
-
-  if ( !template_img_2.data ){
-    printf("No Template data \n");
-    return -1;
-  }
-
-  SIFT sift;
-  vector<Feature> firstResults = sift.run(template_img_1);
-  vector<Feature> secondResults = sift.run(template_img_2);
-
-  // //printing out orientations
-  // cout << "key point orientations" << endl;
-  // for( auto iter= firstResults.begin(); iter != firstResults.end(); iter++ ){
-  //   for( int i= 0; i < iter->second.orientation.size(); i++){
-  //     cout << iter->second.orientation[i] << " " ;
-  //   }
-  //   cout << endl;
-  // }
-
-  ////
-  cout << "Count 1:" << firstResults.size() << endl;
-  cout << "Count 2:" << secondResults.size() << endl;
-
-
-  // Exhaustive search
-  vector<Feature>::iterator firstIt;
-  vector<Feature>::iterator secondIt;
-  int count = 0;
-  for (firstIt = firstResults.begin(); firstIt != firstResults.end(); firstIt++) {
-    Feature first = (*firstIt);
-    Vec<float, 128> firstDescriptor = first.descriptor;
+    SIFT sift;
+    Mat currentFrame, previousFrame;
+    bool success = video.read(previousFrame); //First frame as previous
+    if (!success) {
+        cout << "No frames in video" << endl;
+    }
+    success = video.read(currentFrame); //Second frame as current
+    if (!success) {
+        cout << "No frames in video" << endl;
+    }
+    vector<Feature> currentFeature = sift.run(currentFrame);
+    vector<Feature> previousFeature = sift.run(previousFrame);
+    namedWindow("Unstabilized", WINDOW_AUTOSIZE );
+    namedWindow("Stabilized", WINDOW_AUTOSIZE );
     
-    // Initialize
-    Point firstClose;
-    double firstDistance = -1;
-    Point secondClose;
-    double secondDistance = -1;
-    for (secondIt = secondResults.begin(); secondIt != secondResults.end(); secondIt++) {
-      Feature second = (*secondIt);
-      Vec<float, 128> secondDescriptor = second.descriptor;
-      double distance = norm(firstDescriptor - secondDescriptor);
-      if (distance < firstDistance || firstDistance == -1) {
-        secondDistance = firstDistance;
-        secondClose = firstClose;
-        firstDistance = distance;
-        firstClose = second.location;
-      }
+    while (true) {
+        cout << "Analyzing..." << endl;
+        // Analyze frames
+
+        // Copy current frame to previous
+        currentFrame.copyTo(previousFrame);
+        previousFeature = move(currentFeature);
+        // Read in next frame
+        bool success = video.read(currentFrame);
+        if (!success) {
+            break;
+        }
+        currentFeature = sift.run(currentFrame);
+
+        cout << "Displaying result..." << endl;
+        imshow("Unstabilized", currentFrame);
+        waitKey(0);
     }
 
-    cout << "1: (" << first.location.x << "," << first.location.y << ")" << endl;
-    cout << "2: (" << firstClose.x << "," << firstClose.y << ") d-distance: " << firstDistance << ", p-distance: " << norm(first.location - firstClose) << endl;
-    cout << "3: (" << secondClose.x << "," << secondClose.y << ") d-distance: " << secondDistance << ", p-distance: " << norm(first.location - secondClose) << endl;
-    cout << "ratio: " << (firstDistance / secondDistance) << endl;
-    cout << endl;
-
-    if (norm(first.location - firstClose) < 50) {
-      count++;
-    }
-  }  
-  cout << "Image space distances < 50: " << count << endl;
-
-  sift.featureMapper(template_img_1, firstResults);
-  namedWindow("Display Image1", WINDOW_AUTOSIZE );
-  imshow("Display Image1", template_img_1);
-  sift.featureMapper(template_img_2, secondResults);
-  namedWindow("Display Image2", WINDOW_AUTOSIZE );
-  imshow("Display Image2", template_img_2);
-  waitKey(0);
-
-  return 0;
+    return 0;
 }
